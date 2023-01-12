1274fb5d3a445d21d69cd944c4ca5f9ab27b9a21
Patrick <privat@skyraptor.eu>
2023-01-12 22:23:47 +0000

    Moved parseFilterString to util

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -19,29 +19,7 @@ export default class Download extends Command {
     const filters: EpisodesFilter[] = []
     if (flags.filter) {
       /* Process all filters split by the delimiter */
-      for (const input of flags.filter.split(',')) {
-        /* Match the line with out magic regex */
-        const [match, season, episode, toSeason, toEpisode] = /^S(\d{2})(?:E(\d{2}))?(?:-S(\d{2})(?:E(\d{2}))?)?$/.exec(input) || []
-        
-        /* Verify that the provided input is well formed */
-        if (! match) {
-          throw new Error(`The provided episode filter '${input}' is invalid. Filters have to be one of the following formats S00E00 (episode), S00 (season), S00E00-S00E00 (range), S00-S00E00 (range)`)
-        }
-
-        filters[] = (e: Episode) => {
-          /* Season */
-          if () {
-            if (typeof toSeason === 'number') {
-              /* Make sure the season is in bounds */
-              if (e.season < season || e.season > toSeason) {
-
-              }
-            } else {
-              
-            }
-          }
-        }
-      }
+      
     }
 
     /* Filter database by provided arguments */
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,6 @@
 import {dirname, join, resolve} from 'node:path'
 import {fileURLToPath} from 'node:url'
+import EpisodeFilter from './episode-filter'
 
 export function rootDir(): string {
   return dirname(fileURLToPath(import.meta.url))
@@ -7,4 +8,23 @@ export function rootDir(): string {
 
 export function dataDir(): string {
   return resolve(join(rootDir(), '../data'))
+}
+
+export function parseFilterString(input: string): EpisodeFilter[] {
+  const filters: EpisodeFilter[] = []
+
+  for (const filter of input.split(',')) {
+    /* Match the line with out magic regex */
+    const [match, season, episode, toSeason, toEpisode] = /^S(\d{2})(?:E(\d{2}))?(?:-S(\d{2})(?:E(\d{2}))?)?$/.exec(input) || []
+
+    /* Verify that the provided filter is well formed */
+    if (! match) {
+      throw new Error(`The provided episode filter '${filter}' is invalid. Filters have to be one of the following formats S00E00 (episode), S00 (season), S00E00-S00E00 (range), S00-S00E00 (range)`)
+    }
+
+    /* Initialize and add the filter */
+    filters.push(new EpisodeFilter(Number(season), Number(episode), Number(toSeason), Number(toEpisode)))
+  }
+
+  return filters
 }
