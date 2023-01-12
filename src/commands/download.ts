7ed7f42a309e0c430a52cccddfc75d7b9e894ac3
Patrick <privat@skyraptor.eu>
2023-01-12 22:16:35 +0000

    wip filtering logic

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -1,27 +1,51 @@
-import { Flags } from '@oclif/core'
-import Command from '../core/Command'
-import Database from '../core/Database'
-import Downloader from '../core/Downloader'
+import { Command, Flags } from '@oclif/core'
+import Database from '../database'
 
 export default class Download extends Command {
-  static description = 'Downloads the episodes from southpark.de'
+  static description = 'Downloads episodes from southpark.de'
 
   static examples = [
     '<%= config.bin %> <%= command.id %>',
   ]
 
   static flags = {
-    season: Flags.integer({char: 's', description: 'Season to download'}),
-    episode: Flags.integer({char: 'e', description: 'Episode to download'}),
+    filter: Flags.string({char: 's', description: 'Limit episodes to download like "S01E01,S02E02,S03E04-S06E02,S07,S9-S10E4"'}),
   }
 
   public async run(): Promise<void> {
-    await super.run()
+    const {flags} = await this.parse(Download)
 
-    const {args, flags} = await this.parse(Download)
+    /* Generate filters if they are provided */
+    const filters: EpisodesFilter[] = []
+    if (flags.filter) {
+      /* Process all filters split by the delimiter */
+      for (const input of flags.filter.split(',')) {
+        /* Match the line with out magic regex */
+        const [match, season, episode, toSeason, toEpisode] = /^S(\d{2})(?:E(\d{2}))?(?:-S(\d{2})(?:E(\d{2}))?)?$/.exec(input) || []
+        
+        /* Verify that the provided input is well formed */
+        if (! match) {
+          throw new Error(`The provided episode filter '${input}' is invalid. Filters have to be one of the following formats S00E00 (episode), S00 (season), S00E00-S00E00 (range), S00-S00E00 (range)`)
+        }
+
+        filters[] = (e: Episode) => {
+          /* Season */
+          if () {
+            if (typeof toSeason === 'number') {
+              /* Make sure the season is in bounds */
+              if (e.season < season || e.season > toSeason) {
+
+              }
+            } else {
+              
+            }
+          }
+        }
+      }
+    }
 
     /* Filter database by provided arguments */
-    const episodes = (new Database).getEpisodes(episode => {
+    const episodes = (new Database()).getEpisodes(episode => {
       if (flags.season && episode.season != flags.season) {
         return false
       }
@@ -31,12 +55,12 @@ export default class Download extends Command {
       }
 
       return true
-    });
+    })
 
     /* Initialize and configure the downloader */
-    const downloader = new Downloader(episodes);
+    const downloader = new Downloader(episodes)
 
     /* Download the episodes */
-    await downloader.download();
+    await downloader.download()
   }
 }
--- src/database.ts
+++ src/database.ts
@@ -5,31 +5,27 @@ import {resolve} from "path"
 import {Episode} from "./types/Episode"
 import {depsDir} from "./Util"
 
-export default class Database
-{
-    static baseURL: string = 'https://bumbummen99.github.io/southpark-downloader/database.json'
+export default class Database {
+  static baseURL = 'https://bumbummen99.github.io/southpark-downloader/database.json'
 
-    private data: Episode[] = []
+  private data: Episode[] = []
 
-    constructor() {
-        this.data = JSON.parse(readFileSync(resolve(depsDir(), 'database.json')).toString())
-    }
+  constructor() {
+    this.data = JSON.parse(readFileSync(resolve(depsDir(), 'database.json')).toString())
+  }
 
-    getEpisodes(filter?: EpisodesFilter): Episode[]
-    {
-        if (filter) {
-            return this.data.filter(filter)
-        } else {
-            return this.data
-        }
-    }
+  getEpisodes(filter?: EpisodesFilter): Episode[] {
+    return filter ? this.data.filter((value, index, array) => filter(value, index, array)) : this.data
+  }
 }
 
-export async function download(url: string = Database.baseURL): Promise<void>
-{
-    const response = await axios.get(url, {
-        responseType: 'json'
-    });
+/**
+ * Sync the local database with the database from the GitHub repository
+ */
+export async function sync(url: string = Database.baseURL): Promise<void> {
+  const response = await axios.get(url, {
+    responseType: 'json',
+  })
 
-    await writeFile(resolve(depsDir(), 'database.json'), JSON.stringify(response.data));
+  await writeFile(resolve(depsDir(), 'database.json'), JSON.stringify(response.data))
 }
