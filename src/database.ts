2610e70b2a7cd2232182fde485ecd5960659d3a1
Patrick Henninger <privat@skyraptor.eu>
2023-01-15 00:33:16 +0000

    [Refacotr] further work on database,

--- src/database.ts
+++ src/database.ts
@@ -1,18 +1,16 @@
 import axios from 'axios'
-import { existsSync, readFileSync } from 'node:fs'
+import { existsSync } from 'node:fs'
 import { readFile, writeFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
 import EpisodeFilter from './episode-filter'
-import { depsDir } from './util'
+import { dataDir } from './util'
+
+export const databaseFilePath = resolve(dataDir(), 'database.json')
 
 export default class Database {
   static baseURL = 'https://bumbummen99.github.io/southpark-downloader/database.json'
 
   private data: Episode[] = []
-
-  constructor() {
-    this.data = JSON.parse(readFileSync(resolve(depsDir(), 'database.json')).toString())
-  }
 
   /**
    * Loads the database from the JSON datafile.
@@ -20,12 +18,12 @@ export default class Database {
    */
   public async load(sync = false): Promise<void> {
     /* Sync database with the repo in case we are forced to or the database does not exist locally */
-    if (sync || existsSync('/path/to/database.json')) {
+    if (sync || existsSync(databaseFilePath)) {
       // Load database from the repository
     }
 
     /* Load the local database file */
-    this.data = JSON.parse(await readFile(resolve('/path/to/database.json'), 'utf-8'))
+    this.data = JSON.parse(await readFile(databaseFilePath, 'utf-8'))
   }
 
   public getEpisodes(filters?: EpisodeFilter[]): Episode[] {
@@ -46,5 +44,5 @@ export async function sync(url: string = Database.baseURL): Promise<void> {
     responseType: 'json',
   })
 
-  await writeFile(resolve(depsDir(), 'database.json'), JSON.stringify(response.data))
+  await writeFile(databaseFilePath, JSON.stringify(response.data))
 }
